2b391faa747ac5d294cd0c3ce539f8619bd18450
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-09 20:12:05 -0300

    Criando o componente de tarefa

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <div class="columns">
+            <div class="column is-7">
+                Descrição da Tarefa
+            </div>
+
+            <div class="column">
+                <CronometroComponent :tempoEmSegundos="tempoEmSegundos" />
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CronometroComponent from './Cronometro.vue';
+
+export default defineComponent({
+    name: 'TarefaComponent',
+    components: {
+        CronometroComponent,
+    },
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            default: 0,
+        },
+    },
+});
+</script>
+
+<style scoped>
+.box {
+    background-color: #FAF0CA
+}
+</style>
