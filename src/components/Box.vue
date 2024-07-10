25d59b566d2d39c2f1f816fac966ca3e5b29573d
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-09 21:07:00 -0300

    Criando componente de Caixa

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <!-- O slot é um recurso do Vue que permite que o conteúdo 
+         seja passado para um componente filho. -->
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "BoxComponent",
+});
+</script>
+
+<style scoped>
+.box {
+    background-color: #faf0ca;
+}
+</style>
