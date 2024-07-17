d1b4b093dcf35b7f5e9fc98fb44096619d44a29c
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-17 17:28:29 -0300

    Refatorando a view de projetos para ficar mais coesa

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -3,21 +3,12 @@
     <!-- Classe title e o form usando o framework Bulma -->
     <h1 class="title">Projetos</h1>
 
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label"> Nome do projeto </label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeDoProjeto"
-          id="nomeDoProjeto"
-        />
-      </div>
-
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
 
     <!-- Classe do Bulma para pegar a largura toda e criar uma tabela -->
     <table class="table is-fullwidth">
@@ -25,12 +16,20 @@
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -43,22 +42,10 @@
 
   export default defineComponent({
     name: "ProjetosComponent",
-    data() {
-      return {
-        nomeDoProjeto: ""
-      };
-    },
-    methods: {
-      salvar() {
-        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto); // Chama a mutation
-        this.nomeDoProjeto = ""; // Limpa o campo de texto
-      },
-    },
     setup() {
       const store = useStore(); // Pega a store que formatei e esta com a key
       return {
-        store,
-        projetos: computed(() => store.state.projetos) // Pega o estado projetos
+        projetos: computed(() => store.state.projetos), // Pega o estado projetos
       };
     },
   });
