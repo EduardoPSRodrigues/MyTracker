808d49abb7d969d78c29ef91f4a7f2435d340c11
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-17 13:28:14 -0300

    Criando interface dos projetos - Formulário e função de salvar

---
+++ src/views/Projetos.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="projetos">
+    <!-- Classe title e o form usando o framework Bulma -->
+    <h1 class="title">Projetos</h1>
+
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjeto"
+        />
+      </div>
+
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+  import IProjeto from "@/interfaces/IProjeto";
+import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "ProjetosComponent",
+    data() {
+      return {
+        nomeDoProjeto: "",
+        projetos: [] as IProjeto[],
+      };
+    },
+    methods: {
+      salvar() {
+        const projeto: IProjeto = {
+          nome: this.nomeDoProjeto,
+          id: new Date().toISOString(),
+        };
+        this.projetos.push(projeto); // Adiciona o projeto no array de projetos
+        this.nomeDoProjeto = ""; // Limpa o campo de texto
+      },
+    },
+  });
+</script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
