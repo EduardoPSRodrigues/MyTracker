75bce3a06ac82fc4593886f313739a9f3efe6a57
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-17 13:27:43 -0300

    Criando rotas de navegação
    Pagina Home e Projetos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,23 @@
-import Tarefas from '@/views/Tarefas.vue';
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Projetos from "@/views/Projetos.vue";
+import Tarefas from "@/views/Tarefas.vue";
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tarefas',
-    component: Tarefas
-}]
+const rotas: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Tarefas",
+    component: Tarefas,
+  },
+  {
+    path: "/projetos",
+    name: "Projetos",
+    component: Projetos,
+  },
+];
 
 const roteador = createRouter({
-    history: createWebHashHistory(),
-    routes: rotas
+  history: createWebHashHistory(),
+  routes: rotas,
 });
 
 export default roteador;
