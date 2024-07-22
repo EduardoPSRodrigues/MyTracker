766420ad538f8f099b1f117217165b8bed6b3b95
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-22 10:39:32 -0300

    Atualizando para ser o arquivo pai e receber todas as rotas

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,53 +1,17 @@
 <template>
-  <section class="projetos">
-    <!-- Classe title e o form usando o framework Bulma -->
+  <div class="projetos">
+    <!-- Classe title usando o framework Bulma -->
     <h1 class="title">Projetos</h1>
 
-    <router-link to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i>
-      </span>
-      <span>Novo projeto</span>
-    </router-link>
-
-    <!-- Classe do Bulma para pegar a largura toda e criar uma tabela -->
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <router-link :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
-  import { useStore } from "@/store";
-  import { computed, defineComponent } from "vue";
+  import { defineComponent } from "vue";
 
   export default defineComponent({
     name: "ProjetosComponent",
-    setup() {
-      const store = useStore(); // Pega a store que formatei e esta com a key
-      return {
-        projetos: computed(() => store.state.projetos), // Pega o estado projetos
-      };
-    },
   });
 </script>
 
