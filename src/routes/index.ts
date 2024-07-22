bfc03d2358fcfa9d54287d9207f6b4f8b82f6c10
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-22 10:39:07 -0300

    Agrupando as rotas do projeto

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Projetos from "@/views/Projetos.vue";
 import Tarefas from "@/views/Tarefas.vue";
 import Formulario from "@/views/Projetos/Formulario.vue";
+import Lista from "@/views/Projetos/Lista.vue";
 
 const rotas: RouteRecordRaw[] = [
   {
@@ -11,20 +12,26 @@ const rotas: RouteRecordRaw[] = [
   },
   {
     path: "/projetos",
-    name: "Projetos",
     component: Projetos,
-  },
-  {
-    path: "/projetos/novo",
-    name: "Novo projeto",
-    component: Formulario,
-  },
-  {
-    path: "/projetos/:id",
-    name: "Editar projeto",
-    component: Formulario,
-    props: true, // Passa o parâmetro id para o componente
-  },
+    children: [
+      {
+        path: "",
+        name: "Projetos",
+        component: Lista,
+      },
+      {
+        path: "novo",
+        name: "Novo projeto",
+        component: Formulario,
+      },
+      {
+        path: ":id",
+        name: "Editar projeto",
+        component: Formulario,
+        props: true, // Passa o parâmetro id para o componente
+      },
+    ]
+  }
 ];
 
 const roteador = createRouter({
