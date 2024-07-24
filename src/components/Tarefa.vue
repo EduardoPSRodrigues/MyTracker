8d4aed2ba5ce9dffe93c0bc468ae9e31d5f59640
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-07-24 12:01:53 -0300

    Update Tarefa.vue

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,48 +1,52 @@
 <template>
-    <Box>
-        <div class="columns">
-            <div class="column is-7 ">
-                {{ tarefa.descricao || 'Tarefa sem descrição' }}
-            </div>
+  <Box>
+    <div class="columns">
+      <div class="column is-4">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
 
-            <div class="column resetCSS">
-                <CronometroComponent :tempoEmSegundos="tarefa.duracaoEmSegundos" />
-            </div>
-        </div>
-    </Box>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || "Projeto não disponível." }}
+      </div>
+
+      <div class="column resetCSS">
+        <CronometroComponent :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
 </template>
 
 <script lang="ts">
-//PropType é uma função que é usada para definir o tipo de uma propriedade em um componente Vue.
-import { defineComponent, PropType } from 'vue'; //
-import CronometroComponent from './Cronometro.vue';
-import ITarefa from '@/interfaces/ITarefa';
-import Box from './Box.vue';
+  //PropType é uma função que é usada para definir o tipo de uma propriedade em um componente Vue.
+  import { defineComponent, PropType } from "vue"; //
+  import CronometroComponent from "./Cronometro.vue";
+  import ITarefa from "@/interfaces/ITarefa";
+  import Box from "./Box.vue";
 
-export default defineComponent({
-    name: 'TarefaComponent',
+  export default defineComponent({
+    name: "TarefaComponent",
     components: {
-        CronometroComponent, 
-        Box
+      CronometroComponent,
+      Box,
     },
     props: {
-        // O tipo de propriedade é um objeto que segue a interface ITarefa
-        tarefa: {
-            type: Object as PropType<ITarefa>,
-            required: true,
-        },
-        tempoEmSegundos: {
-            type: Number,
-            default: 0,
-        },
+      // O tipo de propriedade é um objeto que segue a interface ITarefa
+      tarefa: {
+        type: Object as PropType<ITarefa>,
+        required: true,
+      },
+      tempoEmSegundos: {
+        type: Number,
+        default: 0,
+      },
     },
-});
+  });
 </script>
 
 <style scoped>
-.resetCSS  {
+  .resetCSS {
     padding: 0;
     align-self: center;
     margin-left: 6px;
-}
+  }
 </style>
